181efd036a1d3a4f14f9db2a7ca806312d04d681
AzkaSaputraaa <azkasaputra192005@gmail.com>
2024-12-18 08:54:48 +0700

    Test

---
+++ nomor 1.py
@@ -0,0 +1,7 @@
+total = 0
+n = 5  # contoh nilai n
+
+for i in range(1, n + 1):
+    total += i
+
+print(total)
---
+++ nomor 2.py
@@ -0,0 +1,7 @@
+phi = 3.14
+
+r = float(input("Masukkan jari-jari lingkaran: "))
+
+luas = phi * r**2
+
+print(f"Luas lingkaran adalah: {luas}")
